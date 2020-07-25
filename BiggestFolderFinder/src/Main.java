2784010f2466711af7aafe3c49510196df6c3e07
dmitrijsidelnikov <kiberline@yandex.ru>
2020-07-25 12:07:56 +0200

    Inintial

--- BiggestFolderFinder/src/Main.java
+++ BiggestFolderFinder/src/Main.java
@@ -1,5 +1,10 @@
+import java.io.File;
+import java.nio.file.Files;
+
 public class Main {
     public static void main(String[] args) {
         String folderPath = "/Users/dmitrijsidelnikov/Library/Mobile\\ Documents/com~apple~CloudDocs/Java";
+        File file = new File(folderPath);
+        System.out.println(file.length());
     }
 }
