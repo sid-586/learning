8cb7509792445516345d00cce62bd105d1d401d6
dmitrijsidelnikov <kiberline@yandex.ru>
2020-07-25 14:03:04 +0200

    dev 2 methods

---
+++ BiggestFolderFinder/src/MyThread.java
@@ -0,0 +1,13 @@
+public class MyThread extends Thread {
+private int threadNumber;
+public MyThread(int threadNumber) {
+    this.threadNumber = threadNumber;
+}
+
+    @Override
+    public void run() {
+        super.run();
+    }
+
+
+}
