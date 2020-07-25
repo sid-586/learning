c9fabbe4ba37f24f6e97958bfaba401ae0031043
dmitrijsidelnikov <kiberline@yandex.ru>
2020-07-25 12:23:02 +0200

    dev 1

--- BiggestFolderFinder/src/Main.java
+++ BiggestFolderFinder/src/Main.java
@@ -3,8 +3,20 @@ import java.nio.file.Files;
 
 public class Main {
     public static void main(String[] args) {
-        String folderPath = "/Users/dmitrijsidelnikov/Library/Mobile\\ Documents/com~apple~CloudDocs/Java";
+        String folderPath = "/Users/dmitrijsidelnikov/Library/Mobile Documents/com~apple~CloudDocs/Java";
         File file = new File(folderPath);
-        System.out.println(file.length());
+        System.out.println(getFolderSize(file));
+
+    }
+    public static long getFolderSize(File folder) {
+        if (folder.isFile()) {
+            return folder.length();
+        }
+        long sum = 0;
+        File[] files = folder.listFiles();
+        for (File file: files) {
+            sum += getFolderSize(file);
+        }
+        return sum;
     }
 }
